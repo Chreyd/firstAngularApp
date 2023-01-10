ce5ef71183c12de6042fd022fde215a98e1f3b03
Chreyd <chreydmapeba@gmail.com>
2023-01-10 11:11:50 +0100

    template et directive structurelle *ngIf && *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `<p>Hello {{title}} </p>`,
+  templateUrl: './app.component.html',
   styleUrls:[]
 })
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardsComponent } from './cards/cards.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
