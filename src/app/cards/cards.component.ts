db475b06f3609526bf382f1c8381f775d6f0b902
Chreyd <chreydmapeba@gmail.com>
2023-01-10 15:29:37 +0100

    Get, Set avec TypeScript et filtrage d'hôtel

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,65 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 
-import {ICards} from './cards';
+import { ICards } from './cards';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
 })
-export class CardsComponent implements OnInit{
- /*  ngOnInit(): void {
+export class CardsComponent implements OnInit {
+  /*  ngOnInit(): void {
     throw new Error('Method not implemented.');
   } */
-  ngOnInit(): void {
-      console.log('L\'initialization OnInit fonctionne');
-  }
 
   public title: string = 'Titre du card';
 
   public cards: ICards[] = [
     {
       cardsId: 1,
-      cardsName: 'Hotel1',
+      cardsName: 'Marrakech hotel',
       description: 'vue de la première description',
       price: 660.5,
       imageUrl: '../../assets/image1.jpg',
     },
     {
       cardsId: 2,
-      cardsName: 'Hotel2',
+      cardsName: 'Casablanca hotel',
       description: 'vue de la duxième description',
       price: 299.6,
       imageUrl: 'assets/image2.jpg',
     },
     {
       cardsId: 3,
-      cardsName: 'Hotel3',
+      cardsName: 'Settat hotel',
       description: 'vue de la troisième description',
       price: 2090.5,
       imageUrl: 'assets/image3.jpg',
     },
     {
       cardsId: 4,
-      cardsName: 'Hotel4',
+      cardsName: 'Rabat hotel',
       description: 'vue de la quatrième description',
       price: 230.0,
       imageUrl: 'assets/image4.jpg',
     },
     {
       cardsId: 5,
-      cardsName: 'Hotel5',
+      cardsName: 'Mohammedia hotel',
       description: 'vue de la cinquième description',
       price: 28.5,
       imageUrl: 'assets/image5.jpg',
     },
   ];
   public showBadge: boolean = true;
-
-  public toggleBadge():void{
-    this.showBadge=!this.showBadge;
+  public toggleBadge(): void {
+    this.showBadge = !this.showBadge;
   }
 
-  public hotelFilter:string = 'mot';
+  public filteredCards: ICards[] = [];
+  ngOnInit(): void {
+    this.filteredCards = this.cards;
+    this.hotelFilter='mot';
+  }
+
+  private _hotelFilter: string = 'mot';
+  public get hotelFilter(): string{
+    return this._hotelFilter;
+  }
+  public set hotelFilter(filter:string){
+    this._hotelFilter=filter;
+    this.filteredCards= this.hotelFilter ? this.filterHotels(this.hotelFilter): this.cards;
+  /* terner */
+  }
+
+  private filterHotels(criter:string): ICards[]{
+    criter= criter.toLowerCase();
+    const res= this.cards.filter(
+      (hotel:ICards)=> hotel.cardsName.toLowerCase().indexOf(criter)!=-1
+    );
+    return res;
+  }
 
 }
