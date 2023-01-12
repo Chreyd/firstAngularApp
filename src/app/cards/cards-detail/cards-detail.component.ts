9b6a6437a05a1ec6e79e26fcaccd6edc17c80525
Chreyd <chreydmapeba@gmail.com>
2023-01-12 16:18:00 +0100

    routeLinkActive et passage donner de detail

--- src/app/cards/cards-detail/cards-detail.component.ts
+++ src/app/cards/cards-detail/cards-detail.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HotelListService } from '../cards.service';
+import { ICards } from './../cards';
 
 @Component({
   selector: 'app-cards-detail',
   templateUrl: './cards-detail.component.html',
   styleUrls: ['./cards-detail.component.css']
 })
-export class CardsDetailComponent {
+export class CardsDetailComponent implements OnInit{
+  constructor(
+    private route: ActivatedRoute,
+
+    private hotelService: HotelListService
+  ){}
+
+    public hotel: ICards | undefined= <ICards>{};
+
+  ngOnInit(): void {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.hotelService.getCards().subscribe((cards:ICards[])=>{
+      this.hotel = cards.find(hotel=> hotel.cardsId==id) as  ICards;
+      console.log('HOTEL: ',this.hotel );
+    })
+
+/*     const id: number = +!this.route.snapshot.paramMap.get('id');
+ *//*     const id : number = +this.route.snapshot.paramMap. get('id');
+ */  }
+
 
 }
